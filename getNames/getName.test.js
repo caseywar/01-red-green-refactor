1f5ffd38d68c22854d41740f0e6e51dbbd2f2b47
Casey Warren <caseywar1017@gmail.com>
2021-03-22 17:19:11 -0400

    added an test for aang character

--- getNames/getName.test.js
+++ getNames/getName.test.js
@@ -13,3 +13,16 @@ describe('getName testing', () => {
     expect(name).toEqual('spot');
   });
 });
+
+describe('getName testing', () => {
+  it('returns the name property of an object', () => {
+    const character = {
+      _id: '5cf5679a915ecad153ab68c9',
+      name: 'Aang',
+    };
+
+    const name = getName(character);
+
+    expect(name).toEqual('Aang');
+  });
+});
