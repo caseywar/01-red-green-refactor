e4331a1af8a4eba30939902422d760727ef8198c
Casey Warren <caseywar1017@gmail.com>
2021-03-22 17:10:13 -0400

    use bracket notation to get an objects name property

---
+++ getNames/getName.test.js
@@ -0,0 +1,15 @@
+const getName = require('./getName');
+
+describe('getName testing', () => {
+  it('returns the name property of an object', () => {
+    const spot = {
+      name: 'spot',
+      age: 5,
+      weight: '20 lbs',
+    };
+
+    const name = getName(spot);
+
+    expect(name).toEqual('spot');
+  });
+});
